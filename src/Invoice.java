33475c6abeafcd51254908e6d3751c9fe06404b4
jonathansoriano <jonathan.s.sanjuan@outlook.com>
2024-10-08 17:53:11 -0400

    Added comments to I can make sense of it later for review

--- src/Invoice.java
+++ src/Invoice.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 public class Invoice extends JFrame {
-    private String title = "";
+    private String title = "INVOICE";
     private double grandTotal = 0.0;
     private ArrayList<LineItem> items = new ArrayList<>();
 
@@ -26,8 +26,8 @@ public class Invoice extends JFrame {
 
 
     public Invoice(){
-        mainPanel = new JPanel(new BorderLayout()); //
-        this.add(mainPanel);
+        mainPanel = new JPanel(new BorderLayout()); // Allows me to use North, Center, South locations on the Panel.
+        this.add(mainPanel); // Adding Main panel (hold the other 3 panels) to the JFrame (Invoice).
 
         generateInputPanel();
         mainPanel.add(inputPanel, BorderLayout.NORTH);
@@ -41,7 +41,8 @@ public class Invoice extends JFrame {
 
         this.setSize(500, 400);//600 height
         this.setTitle("Invoice Application");
-        this.setLocationRelativeTo(null);
+        this.setLocationRelativeTo(null); // Centers frame to the center of the computer screen
+        this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
@@ -68,6 +69,7 @@ public class Invoice extends JFrame {
         buttonPanel = new JPanel();
 
         JButton clearButton = new JButton("Clear Items");
+        clearButton.setFocusable(false);
         clearButton.addActionListener((ActionEvent ae) -> {
             productTF.setText("");
             quantityTF.setText("");
@@ -75,38 +77,41 @@ public class Invoice extends JFrame {
         });
 
         JButton addItemButton = new JButton("Add Item");
+        addItemButton.setFocusable(false);
         addItemButton.addActionListener((ActionEvent ae) ->{
-
+            // Since we are going to convert text to int and doubles, we need to use the Integer.parseInt() method and Double.parseDouble() method
+            // That being said, if the user puts in the wrong int or double format type, it will throw an exception (NumberFormatException).
+            //This is why we need to use Try Catch block to deal with Exception.
             try {
-                // Code that might throw an exception
-                if (productTF.getText().isEmpty() || quantityTF.getText().isEmpty() || priceTF.getText().isEmpty()){
-                    JOptionPane.showMessageDialog(this, "Please enter values for all the text fields");
+                if (productTF.getText().isEmpty() || quantityTF.getText().isEmpty() || priceTF.getText().isEmpty()){ // If the text fields are empty, show a dialog box to fill text fields to continue
+                    JOptionPane.showMessageDialog(this, "Please enter values for all the text fields");// Parent component is this JFrame(Invoice)
                 }else {
-                    LineItem item = new LineItem(productTF.getText(), Integer.parseInt(quantityTF.getText()), Double.parseDouble(priceTF.getText()));// Do we need to make a reference for every
-                    // item in the LineItem arrayList? Probaby need to so we can use each subtotal
-                    // from each item to get grandtotal
-                    //item.get(index).getProductSubtotal to get a specific item's subtotal from the LineItem arrayList. (it should work anyways)
-                    items.add(new LineItem(productTF.getText(), Integer.parseInt(quantityTF.getText()), Double.parseDouble(priceTF.getText())));
+                    LineItem item = new LineItem(productTF.getText(), Integer.parseInt(quantityTF.getText()), Double.parseDouble(priceTF.getText()));
+                    // We need an instance of the LineItem class every time we add
+                    //an item to the ArrayList called "items" (Line 9)
+                    // We need a new instance for each item added so we can get and use each subtotal
+                    // from each item to get grandTotal.
+                    items.add(new LineItem(productTF.getText(), Integer.parseInt(quantityTF.getText()), Double.parseDouble(priceTF.getText()))); // adding to the ArrayList using LineItem contructor
 
                 }
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException e) { // Code that might throw an exception
                 JOptionPane.showMessageDialog(this, "Please enter valid text into the fields!");
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(this, "Something went wrong...");
             }
-            //Need an if statement.
 
 
 
         });
         JButton generateInvoiceButton = new JButton("Generate Invoice");
+        generateInvoiceButton.setFocusable(false);
         generateInvoiceButton.addActionListener((ActionEvent ae) -> {
             if (productTF.getText().isEmpty() || quantityTF.getText().isEmpty() || priceTF.getText().isEmpty()){
                 JOptionPane.showMessageDialog(this, "Please enter values for all the text fields");
             }
             else {
                 String invoice = "";
-                invoice += String.format("%50s\n", "INVOICE");
+                invoice += String.format("%50s\n", title); //Line 8
                 invoice += "Sam's Small Appliances\n100 Main Street\nAnytown, CA 98765\n";
                 invoice += "=================================================================================\n";
                 invoice += String.format("%s%20s%20s%20s\n\n","Product", "Quantity","Price","Subtotal");
@@ -116,13 +121,13 @@ public class Invoice extends JFrame {
                 }
                 invoice += "=================================================================================\n";
                 for (int i = 0; i < items.size(); i++){
+                    //item.get(index).getProductSubtotal() wil work since the objects in the ArrayList are type LineItem and this method is available to the LineItem class and objects
                     grandTotal += items.get(i).getProductSubtotal();
                 }
-                //System.out.println(grandTotal);// use String.format to add it to the TextArea
                 invoice += "Amount due: " + "$" + grandTotal + "\n";
 
                 invoiceTA.setText(invoice);
-                items.clear();
+                items.clear();// Empties all items in "items" ArrayList.
                 grandTotal = 0.0; // Reset Grand total so if Generate Button is pressed again, the value of grandTotal doesn't increment
                                 //through the for loop.
             }
@@ -136,7 +141,7 @@ public class Invoice extends JFrame {
 
     public void generateInvoicePanel(){
         invoicePanel = new JPanel();
-        invoiceTA = new JTextArea(10, 40);
+        invoiceTA = new JTextArea(10, 40); //height(rows) and length(columns)
         invoiceTA.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(invoiceTA);
 
