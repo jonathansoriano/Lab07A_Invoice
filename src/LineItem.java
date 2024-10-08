3660813b7f5296a07ce72255e2285c3eb0ad57fb
jonathansoriano <jonathan.s.sanjuan@outlook.com>
2024-10-08 17:59:02 -0400

    Added comments to I can make sense of it later for review

--- src/LineItem.java
+++ src/LineItem.java
@@ -1,9 +1,10 @@
-public class LineItem extends Product {
+public class LineItem extends Product {//Extends Product so we can access the Product Name and price
     private int productQuantity = 0;
     private double productSubtotal = 0.0;
 
+    //Super keyword shows inheritance!
     public LineItem(String name, int productQuantity, double price) {
-        super(name, price);
+        super(name, price);// inherited attributes. No need to type out get or set methods for these since we also inherited those methods from Product class
         this.productQuantity = productQuantity;
     }
 
@@ -16,11 +17,11 @@ public class LineItem extends Product {
     }
 
     public double getProductSubtotal(){
-        return productSubtotal = productQuantity * getPrice();
+        return productSubtotal = productQuantity * super.getPrice();
     }
 
     @Override
-    public String toString() {
+    public String toString() { // This returns a String of the information of the instance of that LineItem
         return String.format("%s%20s%20s%20s",getName() , getProductQuantity() ,"$" + getPrice(), "$" + getProductSubtotal());
     }
 }
